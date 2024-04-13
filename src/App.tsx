0beac83f81e9eb0a91ec46d4a4191d628c68f244
Guilherme Cesário <guilhermecesario1808@gmail.com>
2024-04-12 23:30:05 -0300

    uso de dados do arquivo json para exibir rota e marcador

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import MapView from 'react-native-maps';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import MapView, {Marker, Polyline} from 'react-native-maps';
+import gps from './assets/data/frontend_data_gps.json';
 
 function App(): React.JSX.Element {
+  const gpsData = gps.courses[0].gps;
+  const filteredData = gpsData.map(({latitude, longitude}) => ({
+    latitude,
+    longitude,
+  }));
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <MapView provider='google'
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}/>
+      <MapView
+        provider="google"
+        style={styles.map}
+        region={{
+          latitude: filteredData[0].latitude,
+          longitude: filteredData[0].longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}>
+        <Polyline
+          coordinates={filteredData}
+          strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+          strokeColors={[
+            '#7F0000',
+            '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+            '#B24112',
+            '#E5845C',
+            '#238C23',
+            '#7F0000',
+          ]}
+          strokeWidth={3}
+        />
+        <Marker
+          anchor={{x: 0.5, y: 0.5}}
+          coordinate={filteredData[0]}
+          image={require('./assets/img/car1.png')}
+        />
+      </MapView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   map: {
-    ...StyleSheet.absoluteFillObject
-  }
+    ...StyleSheet.absoluteFillObject,
+  },
 });
 
 export default App;
