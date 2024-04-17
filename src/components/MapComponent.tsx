0f047f02c047ab0b50fd5fa5c2160b64a41c9442
Guilherme Cesário <guilhermecesario1808@gmail.com>
2024-04-16 23:12:10 -0300

    opção de trocar tipo de mapa

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,30 +1,49 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {StyleSheet} from 'react-native';
-import MapView, {MarkerAnimated, Polyline} from 'react-native-maps';
+import MapView, {
+  MapOverlay,
+  MapType,
+  MarkerAnimated,
+  Overlay,
+  Polyline,
+} from 'react-native-maps';
 import {RouteContext} from '../contexts/route';
+import {IconButton} from 'react-native-paper';
 
 const MapComponent: React.FC = () => {
   const RouteProvider = useContext(RouteContext);
   const gpsData = RouteProvider.currentCourse.gps;
+  const [mapType, setMapType] = useState<MapType>('standard');
 
   return (
-    <MapView
-      provider="google"
-      style={styles.map}
-      region={{
-        latitude: gpsData[0].latitude,
-        longitude: gpsData[0].longitude,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}>
-      <Polyline coordinates={gpsData} strokeColor="#000" strokeWidth={3} />
-      <MarkerAnimated
-        style={styles.marker}
-        anchor={{x: 0.5, y: 0.5}}
-        coordinate={RouteProvider.coordinate}
-        image={RouteProvider.markerSprite}
+    <>
+      <MapView
+        mapType={mapType}
+        provider="google"
+        style={styles.map}
+        region={{
+          latitude: gpsData[0].latitude,
+          longitude: gpsData[0].longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}>
+        <Polyline coordinates={gpsData} strokeColor="#000" strokeWidth={3} />
+        <MarkerAnimated
+          style={styles.marker}
+          anchor={{x: 0.5, y: 0.5}}
+          coordinate={RouteProvider.coordinate}
+          image={RouteProvider.markerSprite}
+        />
+      </MapView>
+      <IconButton
+        icon={'layers'}
+        style={styles.layerBurron}
+        size={30}
+        onPress={() => {
+          setMapType(mapType === 'standard' ? 'hybrid' : 'standard');
+        }}
       />
-    </MapView>
+    </>
   );
 };
 
@@ -34,9 +53,10 @@ const styles = StyleSheet.create({
   },
   layerBurron: {
     position: 'absolute',
-    top: 30,
-    right: 30,
+    top: 20,
+    right: 20,
     borderRadius: 50,
+    backgroundColor: '#fff',
   },
   marker: {height: 60, width: 60},
 });
