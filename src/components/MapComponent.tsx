a8f3f4941e85ad395a3b95e4492984b4530a0597
Guilherme Cesário <guilhermecesario1808@gmail.com>
2024-04-16 22:56:41 -0300

    funções do mapa passadas para o context de rota

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,71 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet} from 'react-native';
-import MapView, {
-  AnimatedRegion,
-  MarkerAnimated,
-  Polyline,
-} from 'react-native-maps';
+import MapView, {MarkerAnimated, Polyline} from 'react-native-maps';
 import {RouteContext} from '../contexts/route';
 
 const MapComponent: React.FC = () => {
   const RouteProvider = useContext(RouteContext);
   const gpsData = RouteProvider.currentCourse.gps;
-  const sprites: any = {
-    '0': require('../assets/img/car3.png'),
-    '45': require('../assets/img/car4.png'),
-    '90': require('../assets/img/car5.png'),
-    '135': require('../assets/img/car6.png'),
-    '180': require('../assets/img/car7.png'),
-    '225': require('../assets/img/car8.png'),
-    '270': require('../assets/img/car1.png'),
-    '315': require('../assets/img/car2.png'),
-    '360': require('../assets/img/car3.png'),
-  };
-  const [currentLocation, setCurrentLocation] = useState<number>(0);
-  const [markerSprite, setMarkerSprite] = useState(sprites['0']);
-  const [coordinate] = useState(
-    new AnimatedRegion({
-      latitude: gpsData[0].latitude,
-      longitude: gpsData[0].longitude,
-    }),
-  );
-
-  useEffect(() => {
-    setCurrentLocation(0);
-
-    const interval = setInterval(() => {
-      setCurrentLocation(prevLocation =>
-        prevLocation < gpsData.length - 1 ? prevLocation + 1 : 0,
-      );
-    }, 1000 * 1);
-
-    return () => clearInterval(interval);
-  }, [RouteProvider.currentCourse]);
-
-  useEffect(() => {
-    animateMarker();
-    updateMarkerSprite();
-  }, [currentLocation]);
-
-  const animateMarker = () => {
-    const {latitude, longitude} = gpsData[currentLocation];
-    coordinate
-      .timing({
-        latitude,
-        longitude,
-        toValue: 0,
-        useNativeDriver: false,
-        latitudeDelta: 0,
-        longitudeDelta: 0,
-      })
-      .start();
-  };
-
-  const updateMarkerSprite = () => {
-    const direction = gpsData[currentLocation].direction;
-    const nearestAngle = Math.round(direction / 45) * 45;
-    setMarkerSprite(sprites[nearestAngle.toString()]);
-  };
 
   return (
     <MapView
@@ -81,8 +21,8 @@ const MapComponent: React.FC = () => {
       <MarkerAnimated
         style={styles.marker}
         anchor={{x: 0.5, y: 0.5}}
-        coordinate={coordinate}
-        image={markerSprite}
+        coordinate={RouteProvider.coordinate}
+        image={RouteProvider.markerSprite}
       />
     </MapView>
   );
--- src/contexts/route.tsx
+++ src/contexts/route.tsx
@@ -1,10 +1,13 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import {ICourseData, IRouteData} from '../Interfaces';
 import gps from '../assets/data/frontend_data_gps.json';
+import {AnimatedRegion} from 'react-native-maps';
 
 interface IRouteProvider {
   route: IRouteData;
   currentCourse: ICourseData | undefined;
+  markerSprite: any;
+  coordinate: AnimatedRegion;
   setCourse(course: ICourseData): void;
 }
 
@@ -15,13 +18,74 @@ export const RouteProvider = ({children}: any) => {
     gps.courses[0],
   );
   const [route, setRoute] = useState(gps);
+  const sprites: any = {
+    '0': require('../assets/img/car3.png'),
+    '45': require('../assets/img/car4.png'),
+    '90': require('../assets/img/car5.png'),
+    '135': require('../assets/img/car6.png'),
+    '180': require('../assets/img/car7.png'),
+    '225': require('../assets/img/car8.png'),
+    '270': require('../assets/img/car1.png'),
+    '315': require('../assets/img/car2.png'),
+    '360': require('../assets/img/car3.png'),
+  };
+  const [currentLocation, setCurrentLocation] = useState<number>(0);
+  const [markerSprite, setMarkerSprite] = useState(sprites['0']);
+  const [coordinate] = useState(
+    new AnimatedRegion({
+      latitude: currentCourse.gps[0].latitude,
+      longitude: currentCourse.gps[0].longitude,
+    }),
+  );
+
+  useEffect(() => {
+    setCurrentLocation(0);
+
+    const interval = setInterval(() => {
+      setCurrentLocation(prevLocation =>
+        prevLocation < currentCourse.gps.length - 1 ? prevLocation + 1 : 0,
+      );
+    }, 1000 * 3);
+
+    if (currentLocation >= currentCourse.gps.length - 1) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [currentCourse]);
+
+  useEffect(() => {
+    animateMarker();
+    updateMarkerSprite();
+  }, [currentLocation]);
 
   const setCourse = (course: ICourseData) => {
     setCurrentCourse(course);
   };
 
+  const animateMarker = () => {
+    const {latitude, longitude} = currentCourse.gps[currentLocation];
+    coordinate
+      .timing({
+        latitude,
+        longitude,
+        toValue: 0,
+        useNativeDriver: false,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      })
+      .start();
+  };
+
+  const updateMarkerSprite = () => {
+    const direction = currentCourse.gps[currentLocation].direction;
+    const nearestAngle = Math.round(direction / 45) * 45;
+    setMarkerSprite(sprites[nearestAngle.toString()]);
+  };
+
   return (
-    <RouteContext.Provider value={{currentCourse, route, setCourse}}>
+    <RouteContext.Provider
+      value={{currentCourse, route, markerSprite, coordinate, setCourse}}>
       {children}
     </RouteContext.Provider>
   );
